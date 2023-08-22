dfe06d09e6140ca7a4a95c49815c6939412a878e
SirMissir <pkv_96@icloud.com>
2023-08-22 20:38:12 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+    return (
+        <div className='app-wrapper'>
+            HELLO
+        </div>
+    );
+}
+
+
+export default App;
