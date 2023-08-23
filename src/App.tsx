825ccf9bd317f3da24779c4c0f4dafa62d659928
SirMissir <pkv_96@icloud.com>
2023-08-23 22:54:59 +0300

    feat: React кабзда как подробно 01

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,30 @@ function App() {
             HELLO
         </div>
     );
+
+}function Rating () {
+    return (
+        <div className='app-wrapper'>
+            <div>Star</div>
+            <div>Star</div>
+            <div>Star</div>
+            <div>Star</div>
+            <div>Star</div>
+        </div>
+    );
+}
+
+function Accordion(){
+    return(
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
 }
 
 
