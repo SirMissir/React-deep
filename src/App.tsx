58d6e68eb87126c3e8d64f74cae8b22d92b93d81
SirMissir <pkv_96@icloud.com>
2023-08-29 21:02:32 +0300

    feat: React кабзда как подробно 02

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,80 @@
 import React from 'react';
 import './App.css';
 
+function hello(){
 
+    alert ('Hello IT-camasutra')
+};
+
+hello();
 
 function App() {
+    console.log("App rendered")
     return (
         <div className='app-wrapper'>
-            HELLO
-        </div>
-    );
-
-}function Rating () {
-    return (
-        <div className='app-wrapper'>
-            <div>Star</div>
-            <div>Star</div>
-            <div>Star</div>
-            <div>Star</div>
-            <div>Star</div>
+            <AppTitle/>
+            <Rating/>
+            <Accordion/>
         </div>
     );
 }
 
+function AppTitle () {
+    console.log("AppTitle rendered")
+    return (
+        <div>
+            This is APP component !
+        </div>
+
+    );
+}
+
+function Rating () {
+    console.log("Rating rendered")
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+    </div>
+
+    );
+}
+
+function Star(){
+    console.log("Star rendered")
+    return(
+        <div>Star</div>
+    )
+}
+
 function Accordion(){
+    console.log("Accordion rendered")
     return(
         <div>
-            <h3>Menu</h3>
-            <ul>
-                <li>1</li>
-                <li>2</li>
-                <li>3</li>
-            </ul>
+            <AccordionTitle/>
+            <AccordionBody/>
         </div>
     )
 }
 
+function AccordionTitle(){
+    console.log("AccordionTitle rendered")
+    return(
+        <h3>Menu</h3>
+    )
+}
+
+function AccordionBody(){
+    console.log("AccordionBody rendered")
+    return(
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+        </ul>
+    )
+}
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,6 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-    <div>Hello</div>,
+    <App />,
   document.getElementById('root')
 );
