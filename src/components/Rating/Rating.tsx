24cba74a6561ab8514d303b591d7f7cfe5c1ac8e
SirMissir <pkv_96@icloud.com>
2023-09-01 23:54:59 +0300

    feat: React кабзда как подробно 03

--- src/components/Rating/Rating.tsx
+++ src/components/Rating/Rating.tsx
@@ -2,13 +2,68 @@ import React from "react";
 
 export function Rating( props: any) {
     console.log("Rating rendered")
-    
-    return (
+
+    if (props.value === 1){ return (
         <div>
             <Star selected = {true}/>
             <Star selected = {false}/>
+            <Star selected = {false}/>
+            <Star selected = {false}/>
+            <Star selected = {false}/>
+        </div>
+
+    );
+    }
+    if (props.value === 2){ return (
+        <div>
             <Star selected = {true}/>
             <Star selected = {true}/>
+            <Star selected = {false}/>
+            <Star selected = {false}/>
+            <Star selected = {false}/>
+        </div>
+
+    );
+    }
+    if (props.value === 3){ return (
+        <div>
+            <Star selected = {true}/>
+            <Star selected = {true}/>
+            <Star selected = {true}/>
+            <Star selected = {false}/>
+            <Star selected = {false}/>
+        </div>
+
+    );
+    }
+    if (props.value === 4){ return (
+        <div>
+            <Star selected = {true}/>
+            <Star selected = {true}/>
+            <Star selected = {true}/>
+            <Star selected = {true}/>
+            <Star selected = {false}/>
+        </div>
+
+    );
+    }
+    if (props.value === 5){ return (
+        <div>
+            <Star selected = {true}/>
+            <Star selected = {true}/>
+            <Star selected = {true}/>
+            <Star selected = {true}/>
+            <Star selected = {true}/>
+        </div>
+
+    );
+    }
+    return (
+        <div>
+            <Star selected = {false}/>
+            <Star selected = {false}/>
+            <Star selected = {false}/>
+            <Star selected = {false}/>
             <Star selected = {false}/>
         </div>
 
