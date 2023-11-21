121b060d873e7f7efc67eadee1bb9684a9cd1ca3
SirMissir <pkv_96@icloud.com>
2023-11-21 23:24:47 +0300

    feat: React кабзда как подробно 08 Callback

--- src/components/UnControlledRating/UnControlledRating.tsx
+++ src/components/UnControlledRating/UnControlledRating.tsx
@@ -13,21 +13,18 @@ export function UnControlledRating() {
 
 return(
     <div>
-        <Star selected = {value > 0}/><button   onClick={()=>{setValue(1)}}>1</button>
-        <Star selected = {value > 1}/><button   onClick={()=>{setValue(2)}}>2</button>
-        <Star selected = {value > 2}/><button   onClick={()=>{setValue(3)}}>3</button>
-        <Star selected = {value > 3}/><button   onClick={()=>{setValue(4)}}>4</button>
-        <Star selected = {value > 4}/><button   onClick={()=>{setValue(5)}}>5</button>
-        <Star selected = {value > 5}/><button   onClick={()=>{setValue(6)}}>6</button>
+        <Star selected = {value > 0} setValue = {setValue}/>
+        <Star selected = {value > 1} setValue = {setValue}/>
+        <Star selected = {value > 2} setValue = {setValue}/>
+        <Star selected = {value > 3} setValue = {setValue}/>
+        <Star selected = {value > 4} setValue = {setValue}/>
+        <Star selected = {value > 5} setValue = {setValue}/>
     </div>
 )
 }
 
 function Star(props:StarPropsType) {
-    console.log("Star rendered")
-    if(props.selected === true) {
-        return <span><b>Star </b></span>
-    }else{
-       return <span>Star </span>
-    }
+
+    return  <span> { props.selected ? <b>star</b> : "star"}</span>
+
 }
