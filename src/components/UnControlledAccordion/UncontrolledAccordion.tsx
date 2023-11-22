fa81f6b1107dc43cad71fe5983ac00d78e15c4f5
SirMissir <pkv_96@icloud.com>
2023-11-22 17:26:04 +0300

    feat: React кабзда как подробно 08 Callback

--- src/components/UnControlledAccordion/UncontrolledAccordion.tsx
+++ src/components/UnControlledAccordion/UncontrolledAccordion.tsx
@@ -2,28 +2,29 @@ import React, {useState} from "react";
 
 type AccordionPropsType = {
     titleValue: string
-    // collapsed: boolean
 }
 
 function UnControlledAccordion (props:AccordionPropsType){
     console.log("Accordion rendered")
 
-    // const collapsed = false;
     let [collapsed, setCollapsed] = useState(true)
+
     return <div>
-        <AccordionTitle title={props.titleValue}/><button onClick={()=>{setCollapsed(!collapsed) }}>TOGGLE</button>
+        <AccordionTitle title={props.titleValue} onClick={ ()=>{setCollapsed(!collapsed)} }/>
+
         {!collapsed  && <AccordionBody />}
     </div>
 }
 
 type AccordionTitlePropsType = {
     title: string
+    onClick: ()=>void
 }
 
 function AccordionTitle(props:AccordionTitlePropsType){
     console.log("AccordionTitle rendered")
     return(
-        <h3>{props.title}</h3>
+        <h3 onClick={() => { props.onClick }}>{props.title}</h3>
     )
 }
 
